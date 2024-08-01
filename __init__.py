06be2ed0c4fd17d17f77c0950617707a97056f1c
Sophia Yeung <yhwhiuwai@gmail.com>
2024-08-01 15:50:39 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,26 @@
+'''
+Python package mainly store
+.py files
+Directory can be used to store
+any types of files
+.py, .doc, .jpg, etc.
+'''
+name = '99'
+name = "99"
+name = '''99'''
+# multiple lines of codes
+# Double quotation marks " not ''
+print(f'name = {name}, type(name) = {type(name)}')
+#name = 99, type(name) = <class 'str'>, str = string
+
+# storing multiple types of data
+names = ['Sophia', 'Mary', 'Ben', 'Davis']
+print(f'names = {names}, type(names) = {type(names)}')
+# can store a list of names
+
+# replace ( from [
+names = ('Sophia', 'Mary', 'Ben', 'Davis')
+print(f'names = {names}, type(names) = {type(names)}')
+names= ('1', '2', '3', '4')
+print(f'names = {names}, type(names) = {type(names)}')
+#it covers the previous data
