6e3344c0e349c5ec9af9027a9407150b9cfaa0a8
Sophia Yeung <yhwhiuwai@gmail.com>
2024-08-01 18:41:57 +0800

    Add files via upload

---
+++ Project 3.py
@@ -0,0 +1,16 @@
+#list, tuple, set
+names = ('Sophia', 'Ben', 'Mary', 'Mary')
+print(f'names = {names}, type(names) = {type(names)}')
+
+names = ['Sophia', 'Ben', 'Mary', 'Mary']
+print(f'names = {names}, type(names) = {type(names)}')
+
+#the output doesn't follow the initial arrangement
+#but they do follow the inner operating arrangement
+names = {'Sophia', 'Ben', 'Mary', 'Mary'}
+print(f'names = {names}, type(names) = {type(names)}')
+
+#a list of data
+stud_id = {'name':'Sophia', 'major':'Electrical and Computer Engineering', 'student ID': '228003008'}
+print(f'stud_id = {stud_id}, type(stud_id) = {type(stud_id)}')
+
