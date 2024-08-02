45f5abfac6a47a7d80d8183d665269cb122c9c2f
Sophia Yeung <yhwhiuwai@gmail.com>
2024-08-02 21:26:07 +0800

    Add files via upload

---
+++ test 10.py
@@ -0,0 +1,19 @@
+'''
+operation
+new data cover previous data
+'''
+n = 10
+n += 1
+print(f'n = {n}') #11
+
+n -= 5
+print(f'n = {n}') #6
+
+n *= 5
+print(f'n = {n}') #30
+
+n /= 5
+print(f'n = %1d' %(n)) #6
+
+n %= 5
+print(f'n = {n}') #1
