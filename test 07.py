e722e7382229a68db918f145af11b41ecb9283c9
Sophia Yeung <yhwhiuwai@gmail.com>
2024-08-02 19:10:37 +0800

    Add files via upload

---
+++ test 07.py
@@ -0,0 +1,9 @@
+'''
+t-shirt = $ 56.5
+pant = $ 89.8
+total price of 3 t-shirts and five pants?
+'''
+t_shirt = 56.5
+pant = 89.8
+sum = 3 * t_shirt + 5 * pant
+print(f'total price that you should pay = $%.2f' %(sum))
