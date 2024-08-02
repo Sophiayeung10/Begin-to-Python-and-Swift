79aa39fe183ed13153cb974e4420c8d0966f0353
Sophia Yeung <yhwhiuwai@gmail.com>
2024-08-02 23:00:10 +0800

    Add files via upload

---
+++ test 11.py
@@ -0,0 +1,14 @@
+'''
+commas presentation
+which combines all data in one code
+'''
+num1 = 10
+num2 = 20
+num3 = 30
+print(f'num1 = {num1}, num2 = {num2}, num3 = {num3}')
+#redundant
+
+num1, num2, num3 = 10, 20, 30
+print(f'num1 = {num1}, num2 = {num2}, num3 = {num3}')
+#num1, num2, num3 = (expected 3)
+
