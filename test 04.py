2685d70b18712629000238ad58371574edd7f476
Sophia Yeung <yhwhiuwai@gmail.com>
2024-08-02 18:38:02 +0800

    Add files via upload

---
+++ test 04.py
@@ -0,0 +1,31 @@
+# translate to int()
+num1 = '10'
+num2 = '20'
+n1 = int(num1)
+n2 = int(num2)
+result = n1 + n2
+print(f'result = {result}')
+#added together
+
+num1 = '10'
+num2 = '20'
+result = num1 + num2
+print(f'result = {result}')
+#1020
+
+num1 = 66.6
+num2 = 67
+result = num1 + num2
+print(f'result = {result}')
+#result = 133.6
+
+num1 = 66.6
+num2 = 67
+n1 = int(num1)
+result = n1 + num2
+print(f'result = {result}')
+#result = 133
+
+
+
+
---
+++ test 05.py
@@ -0,0 +1,9 @@
+'''inputs
+Please enter your name: Sophia
+please enter your age: 23
+Hello class. My name is Sophia. I am 23 years old.
+'''
+name = input('Please enter your name: ')
+age = input('please enter your age: ')
+gender = input('Please select your gender: ')
+print(f'Hello class. My name is {name} and I am a {gender}. I am {age} years old this year.')
