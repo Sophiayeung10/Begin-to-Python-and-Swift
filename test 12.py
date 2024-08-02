0ea8957093769be7d0b1cc371d426a277bf2f0e4
Sophia Yeung <yhwhiuwai@gmail.com>
2024-08-02 23:03:47 +0800

    Add files via upload

---
+++ test 12.py
@@ -0,0 +1,9 @@
+num1 = 10
+num2 = 20
+result = num1 != num2
+print(f'result = {result}, type(result) = {type(result)}')
+#True
+
+result = num1 == num2
+print(f'result = {result}, type(result) = {type(result)}')
+#False
