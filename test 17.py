ead326511e203b71352ae8f141eca212eba9699a
Sophia Yeung <yhwhiuwai@gmail.com>
2024-08-03 20:36:36 +0800

    Add files via upload

---
+++ test 17.py
@@ -0,0 +1,9 @@
+score = int(input('The score you obtained: '))
+if score >= 80:
+    print('Excellent!')
+elif score >= 70:
+    print('Great!')
+elif score >= 60:
+    print('Good.')
+else: #don't need to use elif
+    print('It would have been better to have given birth to BBQ pork.')
