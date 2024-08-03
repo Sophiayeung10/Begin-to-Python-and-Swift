21a5d0c073cade4946dc9aa827ab8223d9ec7e9e
Sophia Yeung <yhwhiuwai@gmail.com>
2024-08-03 20:14:47 +0800

    Add files via upload

---
+++ test 15.py
@@ -0,0 +1,6 @@
+#if you are a monster, you should be killed
+monster = input('Are you a monster? Yes or No: ')
+if monster == 'yes' :
+    print('You are killed by warriors.')
+else :
+    print('You are safe...temporatary')
