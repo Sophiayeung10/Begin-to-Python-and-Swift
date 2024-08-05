692312544d598f2e3212aa615240d5df99dd357d
Sophia Yeung <yhwhiuwai@gmail.com>
2024-08-05 23:00:26 +0800

    Add files via upload

---
+++ quiz 03.py
@@ -0,0 +1,5 @@
+#Earth's rotation
+for i in range(1, 2, 1):
+    for j in range(1, 366, 1):
+        print(f'The Earth has completed rotation {j} times.')
+    print(f'The Earth has completed rotation around the sun {i} times.')
