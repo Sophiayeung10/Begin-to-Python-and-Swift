5347947373c6fc2bdf60b65d6ec8142ff6a0fe4b
Sophia Yeung <yhwhiuwai@gmail.com>
2024-08-06 15:31:20 +0800

    Add files via upload

---
+++ quiz 07.py
@@ -0,0 +1,8 @@
+while True:
+    choice = input('Would you marry me? y or n: ')
+    if choice == 'y':
+        print('Great!')
+        break
+    else:
+        print('Say again!')
+print('Lalalala~')
