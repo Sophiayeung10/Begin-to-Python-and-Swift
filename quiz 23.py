985659e29bed331f66c562d4b8a015d0bdc50ec5
Sophia Yeung <yhwhiuwai@gmail.com>
2024-08-07 22:11:20 +0800

    Add files via upload

---
+++ quiz 23.py
@@ -0,0 +1,9 @@
+list = ['Ben', 'Davis', 'Python']
+index = 0
+while index < len(list):
+     element = list[index]
+     print(f'element = {element}')
+     index += 1
+
+name = [1, 2, 3, 4]
+print(name[3])
