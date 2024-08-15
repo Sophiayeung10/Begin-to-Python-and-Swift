403a56f2f5d5c5b27270a598d1e5d2d3a59fdd25
Sophia Yeung <yhwhiuwai@gmail.com>
2024-08-15 22:54:49 +0800

    Add files via upload

---
+++ type 5.py
@@ -0,0 +1,7 @@
+my_list = ['Sophia', 'Sophia', 'Ben', 'ith', 'it', 'ith', 'it', 'best']
+empty_set = set()
+for element in my_list:
+    empty_set.add(element)
+print(my_list) #original
+print(empty_set) #A set using for loop
+
