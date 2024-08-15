37db63e9c02f30223ff899c5b6492e482a18619d
Sophia Yeung <yhwhiuwai@gmail.com>
2024-08-15 23:33:00 +0800

    Add files via upload

---
+++ type 7.py
@@ -0,0 +1,13 @@
+New_class = {
+'Sophia': {
+    'Chinese': 88,
+    'English': 77,
+    'Math': 78
+}, 'Ben': {
+    'Chinese': 99,
+    'English': 77,
+    'Math': 34
+    }
+}
+sophia = New_class['Sophia']
+print(sophia)
